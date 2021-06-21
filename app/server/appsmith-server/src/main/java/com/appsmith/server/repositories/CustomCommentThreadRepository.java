0383c7be5f5ca9d11dbbca6458d53f0ee59aeced
Nayan <nayan@appsmith.com>
2021-06-21 23:52:34 +0600

    -Add the newly added user to existing comment threads

--- app/server/appsmith-server/src/main/java/com/appsmith/server/repositories/CustomCommentThreadRepository.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/repositories/CustomCommentThreadRepository.java
@@ -2,10 +2,11 @@ package com.appsmith.server.repositories;
 
 import com.appsmith.server.acl.AclPermission;
 import com.appsmith.server.domains.CommentThread;
+import com.mongodb.client.result.UpdateResult;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface CustomCommentThreadRepository extends AppsmithRepository<CommentThread> {
-
     Flux<CommentThread> findByApplicationId(String applicationId, AclPermission permission);
-
+    Mono<UpdateResult> updatePolicyUsers(String applicationId, AclPermission permission, String username);
 }
--- app/server/appsmith-server/src/main/java/com/appsmith/server/repositories/CustomCommentThreadRepositoryImpl.java
+++ app/server/appsmith-server/src/main/java/com/appsmith/server/repositories/CustomCommentThreadRepositoryImpl.java
@@ -3,12 +3,16 @@ package com.appsmith.server.repositories;
 import com.appsmith.server.acl.AclPermission;
 import com.appsmith.server.domains.CommentThread;
 import com.appsmith.server.domains.QCommentThread;
+import com.mongodb.client.result.UpdateResult;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.mongodb.core.ReactiveMongoOperations;
 import org.springframework.data.mongodb.core.convert.MongoConverter;
 import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -28,4 +32,13 @@ public class CustomCommentThreadRepositoryImpl extends BaseAppsmithRepositoryImp
         return queryAll(List.of(criteria), permission);
     }
 
+    @Override
+    public Mono<UpdateResult> updatePolicyUsers(String applicationId, AclPermission permission,
+                                                String username) {
+        Criteria criteria = where(fieldName(QCommentThread.commentThread.applicationId)).is(applicationId);
+        Update update = new Update()
+                .addToSet("policies.$[element].users", username)
+                .filterArray(where("element.permission").is(permission.getValue()));
+        return this.mongoOperations.updateMulti(Query.query(criteria), update, CommentThread.class);
+    }
 }
