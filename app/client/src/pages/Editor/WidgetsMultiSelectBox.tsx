98afe6b4220861537b82bb43affbc3bd9670bdfe
root <root@DESKTOP-9GENCK0.localdomain>
2021-06-21 17:06:17 +0530

    add icons

--- app/client/src/pages/Editor/WidgetsMultiSelectBox.tsx
+++ app/client/src/pages/Editor/WidgetsMultiSelectBox.tsx
@@ -1,16 +1,50 @@
-import { get, minBy, maxBy } from "lodash";
+import styled from "styled-components";
 import React, { useMemo } from "react";
 import { useSelector } from "react-redux";
-import styled from "styled-components";
-import { getCanvasWidgets } from "selectors/entitiesSelector";
+import { get, minBy, maxBy } from "lodash";
 
+import { FormIcons } from "icons/FormIcons";
+import { ControlIcons } from "icons/ControlIcons";
 import { getSelectedWidgets } from "selectors/ui";
 import { generateClassName } from "utils/generators";
+import { getCanvasWidgets } from "selectors/entitiesSelector";
 
-const MultiSelectBoundingRect = styled.div`
+const StyledSelectionBox = styled.div`
   position: absolute;
-  border: 1px dashed;
+  border: 1.5px dashed #69b5ff;
+  background-color: rgb(84 132 236 / 6%);
 `;
+
+const StyledActions = styled.div`
+  margin-left: calc(100% + 4px);
+  padding: 5px;
+  width: max-content;
+  background-color: ${(props) => props.theme.colors.appBackground};
+`;
+
+const StyledAction = styled.button`
+  cursor: pointer;
+  height: 28px;
+  width: 28px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  outline: none;
+  border: none;
+  background: transparent;
+  &:hover,
+  &:active,
+  &.active {
+    background: ${(props) => (props.disabled ? "initial" : "#e1e1e1")};
+  }
+  &:focus {
+    outline: none;
+  }
+`;
+
+const CopyIcon = ControlIcons.COPY2_CONTROL;
+const DeleteIcon = FormIcons.DELETE_ICON;
+const CutIcon = ControlIcons.CUT_CONTROL;
 
 interface OffsetBox {
   top: number;
@@ -27,7 +61,7 @@ function WidgetsMultiSelectBox(props: { widgetId: string }): any {
   );
 
   /**
-   * the canvas multibox should only render when:
+   * the multi-selection bounding box should only render when:
    *
    * 1. the widgetID is equal to the parent id of selected widget
    * 2. has common parent
@@ -78,11 +112,10 @@ function WidgetsMultiSelectBox(props: { widgetId: string }): any {
     return {};
   }, [selectedWidgets]);
 
-  // if multiple widgets are not selected, then don't render anything
   if (!shouldRender) return false;
 
   return (
-    <MultiSelectBoundingRect
+    <StyledSelectionBox
       style={{
         left: left?.left,
         top: top?.top,
@@ -91,7 +124,19 @@ function WidgetsMultiSelectBox(props: { widgetId: string }): any {
         width:
           get(width, "left", 0) + get(width, "width", 0) - get(left, "left", 0),
       }}
-    />
+    >
+      <StyledActions>
+        <StyledAction>
+          <CopyIcon color="black" height={16} width={16} />
+        </StyledAction>
+        <StyledAction>
+          <CutIcon color="black" height={16} width={16} />
+        </StyledAction>
+        <StyledAction>
+          <DeleteIcon color="black" height={16} width={16} />
+        </StyledAction>
+      </StyledActions>
+    </StyledSelectionBox>
   );
 }
 
